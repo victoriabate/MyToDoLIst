381f94739a12877af5bfb632730214a0428ad4d5
victoriabate <victoriabate99@gmail.com>
2017-11-03 16:01:26 +0000

    delete button

--- my-todo-list/src/App.js
+++ my-todo-list/src/App.js
@@ -24,14 +24,30 @@ onSubmit = (event) => {
     items: [...this.state.items, this.state.term]
   })
 }
-
+// onClick =  (event) => {
+//   event.preventDefault();
+//   this.setState({
+//     term: '',
+//     items: []
+//   })
+// }
+onClick = (index) => {
+  let newItems = this.state.items.slice();
+  newItems.pop()
+  console.log(newItems);
+  this.setState({
+    items: newItems
+  })
+}
 render() {
    return (
      <div>
-       <form onSubmit = {this.onSubmit}><List items={this.state.items}/>
+       <form onSubmit = {this.onSubmit}>
          <input value = {this.state.term} onChange={this.onChange}/>
          <button type="submit">Add To Do Item</button>
        </form>
+       <List  onClick={this.onClick}items={this.state.items}/>
+       {/* <button type="submit" onClick={this.onClick}>Delete to do List</button> */}
 
      </div>
    );
